1cc07d76569ef0d712978af49cb24713f9e4406d
Memelouse <115847182+memelouse@users.noreply.github.com>
2025-03-19 19:37:51 +0100

    Update task_scheduler.hpp

--- task_scheduler.hpp
+++ task_scheduler.hpp
@@ -7,7 +7,7 @@
 namespace task_scheduler {
 	namespace offsets {
 		// "11 11 11 11 11 11 71 3f 00 00 00 00 00 00" + 0x20
-		static uintptr_t task_scheduler_offset = 0x61E6010;
+		static uintptr_t task_scheduler_offset = 0x627DAE0;
 		static uintptr_t job_name = 0x90;
 
 		namespace renderjob {
